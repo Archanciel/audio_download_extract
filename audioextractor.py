ef57caa6842032eb7d828fc6baded5b221c45ce8
Archanciel <jp.schnyder@gmail.com>
2020-10-03 20:19:36 +0200

    trying more efficient code. No result !

--- audioextractor.py
+++ audioextractor.py
@@ -17,15 +17,17 @@ class AudioExtractor:
 			import moviepy.editor as mp  # not working on Android
 			for file in files:
 				mp4FilePathName = os.path.join(self.targetAudioDir, file)
-				mp3FilePathName = os.path.join(self.targetAudioDir, os.path.splitext(file)[0] + '.mp3')
 				extractStartEndSecondsLists = playlistTimeFrameData.getExtractStartEndSecondsLists(videoIndex)
+				timeFrameIndex = 1
 				for extractStartEndSecondsList in extractStartEndSecondsLists:
 					timeStartSec = extractStartEndSecondsList[0]
 					timeEndSec = extractStartEndSecondsList[1]
 					clip = mp.AudioFileClip(mp4FilePathName).subclip(timeStartSec, timeEndSec)  # disable if do not want any clipping
+					mp3FilePathName = os.path.join(self.targetAudioDir, os.path.splitext(file)[0] + '_' + str(timeFrameIndex) + '.mp3')
 					clip.write_audiofile(mp3FilePathName)
 					clip.close()
-					os.remove(mp4FilePathName)
+					timeFrameIndex += 1
+				os.remove(mp4FilePathName)
 				videoIndex += 1
 		else:
 			for file in files:
@@ -35,4 +37,48 @@ class AudioExtractor:
 				if os.path.isfile(mp3FilePathName):
 					os.remove(mp3FilePathName)
 
+				os.rename(mp4FilePathName, mp3FilePathName)
+	
+	def extractAudioPortionAlt(self, playlistTimeFrameData):
+		# is slower in fact !!!
+		from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+		files = glob.glob(self.targetAudioDir + DIR_SEP + "*.mp4")
+		files.sort(key=os.path.getmtime)
+		videoIndex = 1
+		
+		if playlistTimeFrameData:
+			import moviepy.editor as mp  # not working on Android
+			for file in files:
+				mp4ExtractFilePathName = os.path.join(self.targetAudioDir, 'extr.mp4')
+				extractStartEndSecondsLists = playlistTimeFrameData.getExtractStartEndSecondsLists(videoIndex)
+				timeFrameIndex = 1
+				for extractStartEndSecondsList in extractStartEndSecondsLists:
+					timeStartSec = extractStartEndSecondsList[0]
+					timeEndSec = extractStartEndSecondsList[1]
+					
+					ffmpeg_extract_subclip(file, timeStartSec, timeEndSec, targetname=mp4ExtractFilePathName)
+					extrVideo = mp.VideoFileClip(mp4ExtractFilePathName)
+					audio = extrVideo.audio
+					mp3FilePathName = os.path.join(self.targetAudioDir,
+					                               os.path.splitext(file)[0] + '_' + str(timeFrameIndex) + '.mp3')
+					audio.write_audiofile(mp3FilePathName)
+					audio.close()
+					extrVideo.close()
+					#
+					# clip = mp.AudioFileClip(mp4FilePathName).subclip(timeStartSec,
+					#                                                  timeEndSec)  # disable if do not want any clipping
+					# clip.write_audiofile(mp3FilePathName)
+					# clip.close()
+					timeFrameIndex += 1
+				os.remove(file)
+				os.remove(mp4ExtractFilePathName)
+				videoIndex += 1
+		else:
+			for file in files:
+				mp4FilePathName = os.path.join(self.targetAudioDir, file)
+				mp3FilePathName = os.path.join(self.targetAudioDir, os.path.splitext(file)[0] + '.mp3')
+				
+				if os.path.isfile(mp3FilePathName):
+					os.remove(mp3FilePathName)
+				
 				os.rename(mp4FilePathName, mp3FilePathName)
--- test/testaudioextractor.py
+++ test/testaudioextractor.py
@@ -1,5 +1,5 @@
 import unittest
-import os, sys, inspect, shutil, glob
+import os, sys, inspect, shutil, glob, time
 from io import StringIO
 
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
@@ -59,13 +59,131 @@ class TestAudioExtractor(unittest.TestCase):
 #		fileNameLst = [x.split(DIR_SEP)[-1] for x in glob.glob(downloadDir + DIR_SEP + '*.*')]
 #		self.assertEqual(sorted(['Wear a mask Help slow the spread of Covid-19.mp4']), sorted(fileNameLst))
 		extractedFileList = os.listdir(targetAudioDir)
-		self.assertEqual(['Wear a mask Help slow the spread of Covid-19.mp3'], extractedFileList)
+		self.assertEqual(['Wear a mask Help slow the spread of Covid-19_1.mp3'], extractedFileList)
 
 		from mutagen.mp3 import MP3
 		audio = MP3(targetAudioDir + DIR_SEP + extractedFileList[0])
 		self.assertAlmostEquals(expectedExtractedFileDuration, audio.info.length, delta=0.1)
+	
+	def testExtractAudioPortionLong_dir_contains_one_file_with_one_extract_timeframe(self):
+		playListName = 'moviepy_explore'
+		targetAudioDir = AUDIO_DIR + DIR_SEP + playListName
+		
+		if not os.path.isdir(targetAudioDir):
+			os.mkdir(targetAudioDir)
+		
+		playlistTimeFrameData = PlaylistTimeFrameData()
+		videoIndex = 1
+		startEndSecondsList = [5, 50]
+		expectedExtractedFileDuration = startEndSecondsList[1] - startEndSecondsList[0]
+		playlistTimeFrameData.addExtractStartEndSecondsList(videoIndex, startEndSecondsList)
+		downloadedVideoInfoDictionary = DownloadedVideoInfoDic(targetAudioDir, playListName)
+		
+		# deleting files in downloadDir
+		files = glob.glob(targetAudioDir + DIR_SEP + '*')
+		
+		for f in files:
+			os.remove(f)
+		
+		# restoring mp4 file
+		
+		sourceVideo = 'Magnifique vidéo de Noël.mp4'
+		sourceVideoFilePath = targetAudioDir + DIR_SEP + sourceVideo
+		shutil.copy(
+			'D:\\Development\\Python\\audiodownload\\test\\testData' + DIR_SEP + sourceVideo,
+			sourceVideoFilePath)
+		guiOutput = GuiOutputStub()
+		audioExtractor = AudioExtractor(guiOutput, targetAudioDir, downloadedVideoInfoDictionary)
+		
+		stdout = sys.stdout
+		outputCapturingString = StringIO()
+		sys.stdout = outputCapturingString
+		
+		audioExtractor.extractAudioPortion(playlistTimeFrameData)
+		
+		sys.stdout = stdout
+		
+		#		if os.name == 'posix':
+		#			self.assertEqual(['downloading Wear a mask. Help slow the spread of Covid-19.',
+		#							  '',
+		#							  ''], outputCapturingString.getvalue().split('\n'))
+		#		else:
+		#			self.assertEqual(['downloading Wear a mask. Help slow the spread of Covid-19.',
+		#							  '',
+		#							  ''], outputCapturingString.getvalue().split('\n'))
+		
+		#		fileNameLst = [x.split(DIR_SEP)[-1] for x in glob.glob(downloadDir + DIR_SEP + '*.*')]
+		#		self.assertEqual(sorted(['Wear a mask Help slow the spread of Covid-19.mp4']), sorted(fileNameLst))
+		extractedFileList = os.listdir(targetAudioDir)
+		self.assertEqual(['Magnifique vidéo de Noël_1.mp3'], extractedFileList)
+		
+		from mutagen.mp3 import MP3
+		audio = MP3(targetAudioDir + DIR_SEP + extractedFileList[0])
+		self.assertAlmostEquals(expectedExtractedFileDuration, audio.info.length, delta=0.1)
+	
+	def testExtractAudioPortionAltLong_dir_contains_one_file_with_one_extract_timeframe(self):
+		playListName = 'moviepy_explore'
+		targetAudioDir = AUDIO_DIR + DIR_SEP + playListName
+		
+		if not os.path.isdir(targetAudioDir):
+			os.mkdir(targetAudioDir)
+		
+		playlistTimeFrameData = PlaylistTimeFrameData()
+		videoIndex = 1
+		startEndSecondsList = [5, 50]
+		expectedExtractedFileDuration = startEndSecondsList[1] - startEndSecondsList[0]
+		playlistTimeFrameData.addExtractStartEndSecondsList(videoIndex, startEndSecondsList)
+		downloadedVideoInfoDictionary = DownloadedVideoInfoDic(targetAudioDir, playListName)
+		
+		# deleting files in downloadDir
+		files = glob.glob(targetAudioDir + DIR_SEP + '*')
+		
+		for f in files:
+			os.remove(f)
+		
+		# restoring mp4 file
+		
+		sourceVideo = 'Magnifique vidéo de Noël.mp4'
+		sourceVideoFilePath = targetAudioDir + DIR_SEP + sourceVideo
+		shutil.copy(
+			'D:\\Development\\Python\\audiodownload\\test\\testData' + DIR_SEP + sourceVideo,
+			sourceVideoFilePath)
+		guiOutput = GuiOutputStub()
+		audioExtractor = AudioExtractor(guiOutput, targetAudioDir, downloadedVideoInfoDictionary)
+		
+		stdout = sys.stdout
+		outputCapturingString = StringIO()
+		sys.stdout = outputCapturingString
+		
+		audioExtractor.extractAudioPortionAlt(playlistTimeFrameData)
+		
+		sys.stdout = stdout
+		
+		#		if os.name == 'posix':
+		#			self.assertEqual(['downloading Wear a mask. Help slow the spread of Covid-19.',
+		#							  '',
+		#							  ''], outputCapturingString.getvalue().split('\n'))
+		#		else:
+		#			self.assertEqual(['downloading Wear a mask. Help slow the spread of Covid-19.',
+		#							  '',
+		#							  ''], outputCapturingString.getvalue().split('\n'))
+		
+		#		fileNameLst = [x.split(DIR_SEP)[-1] for x in glob.glob(downloadDir + DIR_SEP + '*.*')]
+		#		self.assertEqual(sorted(['Wear a mask Help slow the spread of Covid-19.mp4']), sorted(fileNameLst))
+		extractedFileList = os.listdir(targetAudioDir)
+		self.assertEqual(['Magnifique vidéo de Noël_1.mp3'], extractedFileList)
+		
+		from mutagen.mp3 import MP3
+		audio = MP3(targetAudioDir + DIR_SEP + extractedFileList[0])
+#		self.assertAlmostEquals(expectedExtractedFileDuration, audio.info.length, delta=0.1)
+
 
 if __name__ == '__main__':
 #	unittest.main()
 	tst = TestAudioExtractor()
-	tst.testExtractAudioPortion_dir_contains_one_file_with_one_extract_timeframe()
+	# ts = time.time()
+	# tst.testExtractAudioPortionAltLong_dir_contains_one_file_with_one_extract_timeframe()
+	# print(time.time() - ts)
+	ts = time.time()
+	tst.testExtractAudioPortionLong_dir_contains_one_file_with_one_extract_timeframe()
+	print(time.time() - ts)
