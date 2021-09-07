c832ff797e376a6be01b7600eb05c52a630bc88a
Archanciel <jp.schnyder@gmail.com>
2021-09-07 18:19:35 +0200

    deleted unused constants. all tst ok.

--- constants.py
+++ constants.py
@@ -6,18 +6,14 @@ SEVERAL_SECONDS = 5
 
 if os.name == 'posix':
 	CONVERT = False
-	AUDIO_DIR = '/storage/emulated/0/Download/Audiobooks'
-	AUDIO_DIR_TEST = AUDIO_DIR + "/test"
-	SINGLE_VIDEO_AUDIO_DIR = '/storage/emulated/0/Download/Audiobooks/Various'
+	AUDIO_DIR_TEST = "/storage/emulated/0/Download/Audiobooks/test"
 	YOUTUBE_DL_FILE_EXT = 'mp3'
 	DIR_SEP = '/'
 	WIN_WIDTH_RATIO = 1
 	WIN_HEIGHT = 800	
 else:
 	CONVERT = False # can be set to True on Windows only
-	AUDIO_DIR = 'D:\\Users\\Jean-Pierre\\Downloads\\Audiobooks'
-	AUDIO_DIR_TEST = AUDIO_DIR + "\\test"
-	SINGLE_VIDEO_AUDIO_DIR = 'D:\\Users\\Jean-Pierre\\Downloads\\Audiobooks\\Various'
+	AUDIO_DIR_TEST = "D:\\Users\\Jean-Pierre\\Downloads\\Audiobooks\\test"
 	YOUTUBE_DL_FILE_EXT = 'mp3'
 	DIR_SEP = '\\'
 	WIN_WIDTH_RATIO = 0.8
