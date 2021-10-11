3e10c5c588cbd210ea31fa8ddef31243b180b816
Archanciel <jp.schnyder@gmail.com>
2021-10-11 16:06:31 +0200

    bug fix

--- audiocontroller.py
+++ audiocontroller.py
@@ -178,9 +178,10 @@ class AudioController:
 
 		:return: downloadVideoInfoDic, accessError
 		"""
-		# ensuring the modified playlist title which will be used to create a
-		# directory has no illegal Windows10 file or dir name character
-		modifiedPlaylistTitle = DirUtil.replaceUnauthorizedDirOrFileNameChars(modifiedPlaylistTitle)
+		if modifiedPlaylistTitle is not None:
+			# ensuring the modified playlist title which will be used to create a
+			# directory has no illegal Windows10 file or dir name character
+			modifiedPlaylistTitle = DirUtil.replaceUnauthorizedDirOrFileNameChars(modifiedPlaylistTitle)
 		
 		downloadVideoInfoDic, accessError = \
 			PlaylistTitleParser.createDownloadVideoInfoDicForPlaylist(audioRootDir=self.configMgr.dataPath,
