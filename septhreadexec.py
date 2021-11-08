07034f5240d243c3e567465f78374c12e6b81676
Archanciel <jp.schnyder@gmail.com>
2021-11-08 17:57:53 +0100

    Update septhreadexec.py

--- septhreadexec.py
+++ septhreadexec.py
@@ -1,4 +1,8 @@
+# Works on Windows and on Android
+
 import threading, time
+from multiprocessing import Process
+
 
 class SepThreadExec:
 	def __init__(self, callerGUI,
@@ -7,41 +11,60 @@ class SepThreadExec:
 	             funcArgs=None,
 	             endFuncArgs=None):
 		self.callerGUI = callerGUI
+		self.endFunc = endFunc
+		self.endFuncArgs = endFuncArgs
 		
-		if endFuncArgs is None:
+		if self.endFuncArgs is None:
 			endFuncArgs = ()
 		if funcArgs is None:
 			funcArgs = {}
 		
 		args = (func, endFunc) + endFuncArgs
 		
-		def _callback(func, endFunc, *a, **kw):
-			func(**kw)
-			
-			if endFunc is not None:
-				endFunc(*a)
-		
-		self.t = threading.Thread(target=_callback, args=args, kwargs=funcArgs)
-		self.t.setName('Exec thread ' + self.t.getName())
-		self.t.daemon = True
-		
-	def start(self):
-		self.t.start()
+		self.p = Process(target=self._callback, args=args, kwargs=funcArgs)
+		self.p.daemon = True
+	
+	def _callback(self, func, endFunc, *a, **kw):
+		func(**kw)
 
-if __name__ == "__main__":
-	def myFunc(name='', age=0):
+		if endFunc is not None:
+			endFunc(*a)
+	
+	def start(self):
+		self.p.start()
+	
+	def stop(self):
+		self.p.terminate()
+	
+	def stopWithEndFunc(self):
+		self.endFunc(*self.endFuncArgs)
+		self.p.terminate()
+
+
+class GUIStub:
+	def displayMsg(self, msg):
+		print('GUIStub' + ' ' + msg)
+	
+	def myFunc(self, name='', age=0):
 		for i in range(5):
 			time.sleep(1)
-			print('My name is {}. I am {} years old'.format(name, age))
+			print('{}: My name is {}. I am {} years old'.format(i + 1, name, age))
 	
-	def myEndFunc(name='', age=0):
+	def myEndFunc(self, name='', age=0):
 		print('MY SURNAME WAS {}. I was {} years old'.format(name.upper(), age))
-		
-	ste = SepThreadExec(callerGUI=None,
-	              func=myFunc,
-	              endFunc=myEndFunc,
-	              funcArgs={'name': 'Jean-Pierre', 'age': 60},
-	              endFuncArgs=('paulo le scientifique', 14))
+
+
+if __name__ == "__main__":
+	gui = GUIStub()
+	
+	ste = SepThreadExec(callerGUI=gui,
+	                    func=gui.myFunc,
+	                    endFunc=gui.myEndFunc,
+	                    funcArgs={'name': 'Jean-Pierre', 'age': 60},
+	                    endFuncArgs=('paulo le scientifique', 14))
 	
 	ste.start()
-	time.sleep(6)
+	time.sleep(3)
+	#ste.stop()
+	ste.stopWithEndFunc()
+	time.sleep(3)
