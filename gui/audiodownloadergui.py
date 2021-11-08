4a10f3d67a58acc61b77dbb25bfc2e8d8d2ed7c3
Archanciel <jp.schnyder@gmail.com>
2021-11-08 17:45:00 +0100

    moved source file

--- gui/audiodownloadergui.py
+++ gui/audiodownloadergui.py
@@ -4,7 +4,6 @@ currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfram
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,parentdir)
 
-import threading
 from os.path import sep
 
 from kivy.app import App
