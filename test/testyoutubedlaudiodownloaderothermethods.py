9fc6d21d542faccfee0e5a05b3e07664425b5a5b
Archanciel <jp.schnyder@gmail.com>
2021-11-26 19:08:17 +0100

    corrected testing an unused method. tsts ok.

--- test/testyoutubedlaudiodownloaderothermethods.py
+++ test/testyoutubedlaudiodownloaderothermethods.py
@@ -15,19 +15,19 @@ class TestYoutubeDlAudioDownloaderOtherMethods(unittest.TestCase):
 	Since testing download consume band width, it is placed in a specific test class.
 	'''
 
-	def testGetPlaylistObjectOrVideoTitleFortUrl(self):
+	def testGetPlaylistObjectAndTitleFortUrl(self):
 		guiOutput = GuiOutputStub()
 		youtubeAccess = YoutubeDlAudioDownloader(guiOutput, DirUtil.getTestAudioRootPath())
 		playlistUrl = "https://www.youtube.com/playlist?list=PLzwWSJNcZTMRxj8f47BrkV9S6WoxYWYDS"
 		expectedPlaylistTitle = 'test_audio_downloader_one_file'
 
-		youtubePlaylist, playlistTitle, videoTitle, accessError = youtubeAccess.getPlaylistObjectOrVideoTitleFortUrl(playlistUrl)
+		youtubePlaylist, playlistTitle, videoTitle, accessError = youtubeAccess.getPlaylistObjectAndTitleFortUrl(playlistUrl)
 
 		self.assertIsNone(accessError)
 		self.assertEqual(expectedPlaylistTitle, playlistTitle,)
 		self.assertIsNone(videoTitle)
 		
-	def testGetPlaylistObjectOrVideoTitleFortUrlInvalidURL(self):
+	def testGetPlaylistObjectAndTitleFortUrlInvalidURL(self):
 		"""
 		pytube.exceptions.RegexMatchError was raised here ...
 		"""
@@ -35,14 +35,14 @@ class TestYoutubeDlAudioDownloaderOtherMethods(unittest.TestCase):
 		youtubeAccess = YoutubeDlAudioDownloader(guiOutput, DirUtil.getTestAudioRootPath())
 		playlistUrl = "https://www.youtube.com/playlist?list=invalid"
 		
-		youtubePlaylist, playlistTitle, videoTitle, accessError = youtubeAccess.getPlaylistObjectOrVideoTitleFortUrl(playlistUrl)
+		youtubePlaylist, playlistTitle, videoTitle, accessError = youtubeAccess.getPlaylistObjectAndTitleFortUrl(playlistUrl)
 		
 		self.assertIsNotNone(accessError)
 		self.assertEqual(AccessError.ERROR_TYPE_SINGLE_VIDEO_URL_PROBLEM, accessError.errorType)
 		self.assertEqual('trying to get the video title for the URL obtained from clipboard did not succeed.\nfailing URL: https://www.youtube.com/playlist?list=invalid\nerror info: regex_search: could not find match for (?:v=|\/)([0-9A-Za-z_-]{11}).*\nnothing to download.', accessError.errorMsg)
 		self.assertIsNone(videoTitle)
 	
-	def testGetPlaylistObjectOrVideoTitleFortUrlInvalidURL_GeeksForGeeks(self):
+	def testGetPlaylistObjectAndTitleFortUrlInvalidURL_GeeksForGeeks(self):
 		"""
 		When starting AudioDownloader after having copied a geeksforgeeks url, the
 		exception raised was pytube.exceptions.VideoUnavailable instead of
@@ -52,7 +52,7 @@ class TestYoutubeDlAudioDownloaderOtherMethods(unittest.TestCase):
 		youtubeAccess = YoutubeDlAudioDownloader(guiOutput, DirUtil.getTestAudioRootPath())
 		playlistUrl = "https://www.geeksforgeeks.org/python-how-to-use-multiple-kv-files-in-kivy/"
 		
-		youtubePlaylist, playlistTitle, videoTitle, accessError = youtubeAccess.getPlaylistObjectOrVideoTitleFortUrl(
+		youtubePlaylist, playlistTitle, videoTitle, accessError = youtubeAccess.getPlaylistObjectAndTitleFortUrl(
 			playlistUrl)
 		
 		self.assertIsNotNone(accessError)
@@ -62,61 +62,61 @@ class TestYoutubeDlAudioDownloaderOtherMethods(unittest.TestCase):
 			accessError.errorMsg)
 		self.assertIsNone(videoTitle)
 	
-	def testGetPlaylistObjectOrVideoTitleFortUrl_one_time_frame_extract(self):
+	def testGetPlaylistObjectAndTitleFortUrl_one_time_frame_extract(self):
 		guiOutput = GuiOutputStub()
 		youtubeAccess = YoutubeDlAudioDownloader(guiOutput, DirUtil.getTestAudioRootPath())
 		playlistUrl = "https://www.youtube.com/playlist?list=PLzwWSJNcZTMSRkN1v6xt75mWsjewlqfL3"
 		expectedPlaylistTitle = 'Test_title_one_time_frame_extract (e01:05:52-01:07:23)'
 
-		youtubePlaylist, playlistTitle, videoTitle, accessError = youtubeAccess.getPlaylistObjectOrVideoTitleFortUrl(playlistUrl)
+		youtubePlaylist, playlistTitle, videoTitle, accessError = youtubeAccess.getPlaylistObjectAndTitleFortUrl(playlistUrl)
 
 		self.assertIsNone(accessError)
 		self.assertEqual(expectedPlaylistTitle, playlistTitle,)	
 		self.assertIsNone(videoTitle)
 
-	def testGetPlaylistObjectOrVideoTitleFortUrl_one_time_frame_suppress(self):
+	def testGetPlaylistObjectAndTitleFortUrl_one_time_frame_suppress(self):
 		guiOutput = GuiOutputStub()
 		youtubeAccess = YoutubeDlAudioDownloader(guiOutput, DirUtil.getTestAudioRootPath())
 		playlistUrl = "https://www.youtube.com/playlist?list=PLzwWSJNcZTMTE_E56D5obclOxWSIbcqNL"
 		expectedPlaylistTitle = 'Test_title_one_time_frame_suppress (s01:05:52-01:07:23)'
 
-		youtubePlaylist, playlistTitle, videoTitle, accessError = youtubeAccess.getPlaylistObjectOrVideoTitleFortUrl(playlistUrl)
+		youtubePlaylist, playlistTitle, videoTitle, accessError = youtubeAccess.getPlaylistObjectAndTitleFortUrl(playlistUrl)
 
 		self.assertIsNone(accessError)
 		self.assertEqual(expectedPlaylistTitle, playlistTitle,)
 		self.assertIsNone(videoTitle)
 
-	def testGetPlaylistObjectOrVideoTitleFortUrl_one_time_frame_extract_suppress(self):
+	def testGetPlaylistObjectAndTitleFortUrl_one_time_frame_extract_suppress(self):
 		guiOutput = GuiOutputStub()
 		youtubeAccess = YoutubeDlAudioDownloader(guiOutput, DirUtil.getTestAudioRootPath())
 		playlistUrl = "https://www.youtube.com/playlist?list=PLzwWSJNcZTMQglE3cl-cJyYQKrdsMvsfO"
 		expectedPlaylistTitle = 'Test_title_one_time_frame_extract_suppress (e01:05:52-01:07:23 s01:15:52-01:17:23)'
 
-		youtubePlaylist, playlistTitle, videoTitle, accessError = youtubeAccess.getPlaylistObjectOrVideoTitleFortUrl(playlistUrl)
+		youtubePlaylist, playlistTitle, videoTitle, accessError = youtubeAccess.getPlaylistObjectAndTitleFortUrl(playlistUrl)
 
 		self.assertIsNone(accessError)
 		self.assertEqual(expectedPlaylistTitle, playlistTitle,)
 		self.assertIsNone(videoTitle)
 
-	def testGetPlaylistObjectOrVideoTitleFortUrl_one_time_frame_suppress_extract(self):
+	def testGetPlaylistObjectAndTitleFortUrl_one_time_frame_suppress_extract(self):
 		guiOutput = GuiOutputStub()
 		youtubeAccess = YoutubeDlAudioDownloader(guiOutput, DirUtil.getTestAudioRootPath())
 		playlistUrl = "https://www.youtube.com/playlist?list=PLzwWSJNcZTMQCd5zf-V9H0ePbDqw3nfbR"
 		expectedPlaylistTitle = 'Test_title_one_time_frame_suppress_extract (s01:05:52-01:07:23 e01:15:52-01:17:23)'
 
-		youtubePlaylist, playlistTitle, videoTitle, accessError = youtubeAccess.getPlaylistObjectOrVideoTitleFortUrl(playlistUrl)
+		youtubePlaylist, playlistTitle, videoTitle, accessError = youtubeAccess.getPlaylistObjectAndTitleFortUrl(playlistUrl)
 
 		self.assertIsNone(accessError)
 		self.assertEqual(expectedPlaylistTitle, playlistTitle,)
 		self.assertIsNone(videoTitle)
 
-	def testGetPlaylistObjectOrVideoTitleFortUrl_multiple_time_frame_extract_suppress(self):
+	def testGetPlaylistObjectAndTitleFortUrl_multiple_time_frame_extract_suppress(self):
 		guiOutput = GuiOutputStub()
 		youtubeAccess = YoutubeDlAudioDownloader(guiOutput, DirUtil.getTestAudioRootPath())
 		playlistUrl = "https://www.youtube.com/playlist?list=PLzwWSJNcZTMQv-CxkvJ0NCti5L9AwLGUr"
 		expectedPlaylistTitle = 'Test_title_multiple_time_frame_suppress_extract (s01:05:52-01:07:23 e01:15:52-01:17:23 S01:25:52-01:27:23 E01:35:52-01:37:23)'
 
-		youtubePlaylist, playlistTitle, videoTitle, accessError = youtubeAccess.getPlaylistObjectOrVideoTitleFortUrl(playlistUrl)
+		youtubePlaylist, playlistTitle, videoTitle, accessError = youtubeAccess.getPlaylistObjectAndTitleFortUrl(playlistUrl)
 
 		self.assertIsNone(accessError)
 		self.assertEqual(expectedPlaylistTitle, playlistTitle,)
@@ -126,4 +126,4 @@ class TestYoutubeDlAudioDownloaderOtherMethods(unittest.TestCase):
 if __name__ == '__main__':
 	#unittest.main()
 	tst = TestYoutubeDlAudioDownloaderOtherMethods()
-	tst.testGetPlaylistObjectOrVideoTitleFortUrlInvalidURL_GeeksForGeeks()
+	tst.testGetPlaylistObjectAndTitleFortUrlInvalidURL_GeeksForGeeks()
