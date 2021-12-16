5794d2fc8577564b154128744b84abadb232d003
Archanciel <jp.schnyder@gmail.com>
2021-12-16 19:03:41 +0100

    removed youtube_dl bug video

--- test/verifytestaudiodownloaderguiurllist.py
+++ test/verifytestaudiodownloaderguiurllist.py
@@ -18,8 +18,8 @@ class VerifyTestAudioDownloaderGUIUrlList(TryTestAudioDownloaderGUIUrlList):
 	def verifyTestAudioDownloaderGUI(self):
 		playlistDirExpectedFileNameLstDic = {}
 		
-		# downloadDir = self.configMgr.dataPath + sep + self.playlistDirName_3
-		# playlistDir_expectedFileNameLst = DirUtil.getFileNamesInDir(downloadDir)
+		downloadDir = self.configMgr.dataPath + sep + self.playlistDirName_7
+		playlistDir_expectedFileNameLst = DirUtil.getFileNamesInDir(downloadDir)
 		
 		# the three videos in the playlist have been partially downloaded
 		playlistDir_3_expectedFileNameLst = ['96-SLS DRIFT TAXI. Arina. #shorts 2021-08-12.mp3',
@@ -40,37 +40,35 @@ class VerifyTestAudioDownloaderGUIUrlList(TryTestAudioDownloaderGUIUrlList):
 		# now verifying the index prefix and upload date suffix automatic
 		# setting for playlist url list downloading
 
-		playlistDir_5_expectedFileNameLst = ['Shmeksss Short Video.mp3',
+		playlistDir_5_expectedFileNameLst = ['Funny suspicious looking dog.mp3',
+		                                     'Here to help - Give him what he wants.mp3',
 		                                     'test warning index date files_noIndexNoDate_dic.txt',
 		                                     'Wear a mask. Help slow the spread of Covid-19..mp3']
 		playlistDirExpectedFileNameLstDic[self.playlistDirName_5] = playlistDir_5_expectedFileNameLst
 		
-		playlistDir_6_expectedFileNameLst = ['92-Shmeksss Short Video.mp3',
+		playlistDir_6_expectedFileNameLst = ['92-Funny suspicious looking dog.mp3',
 		                                     '98-Here to help - Give him what he wants.mp3',
-		                                     'Funny suspicious looking dog.mp3',
 		                                     'test warning index date files_indexNoDate_dic.txt',
 		                                     'Wear a mask. Help slow the spread of Covid-19..mp3']
 		playlistDirExpectedFileNameLstDic[self.playlistDirName_6] = playlistDir_6_expectedFileNameLst
 
 		playlistDir_7_expectedFileNameLst = ['Funny suspicious looking dog 2013-11-05.mp3',
-		                                     'Here to help - Give him what he wants.mp3',
-		                                     'Shmeksss Short Video 2021-01-26.mp3',
-		                                     'test warning index date files_noIndexDate_dic.txt',
-		                                     'Wear a mask. Help slow the spread of Covid-19..mp3']
+		                                      'Here to help - Give him what he wants.mp3',
+		                                      'test warning index date files_noIndexDate_dic.txt',
+		                                      'Wear a mask. Help slow the spread of Covid-19. 2020-07-31.mp3']
 		playlistDirExpectedFileNameLstDic[self.playlistDirName_7] = playlistDir_7_expectedFileNameLst
 
-		playlistDir_8_expectedFileNameLst = ['92-Shmeksss Short Video 2021-01-26.mp3',
+		playlistDir_8_expectedFileNameLst = ['93-Here to help - Give him what he wants 2019-06-07.mp3',
 		                                     '99-Wear a mask. Help slow the spread of Covid-19. 2020-07-31.mp3',
 		                                     'Funny suspicious looking dog.mp3',
-		                                     'Here to help - Give him what he wants.mp3',
 		                                     'test warning index date files_IndexDate_dic.txt']
 		playlistDirExpectedFileNameLstDic[self.playlistDirName_8] = playlistDir_8_expectedFileNameLst
 
-		playlistDir_9_expectedFileNameLst = ['99-Shmeksss Short Video 2021-01-26.mp3',
+		playlistDir_9_expectedFileNameLst = ['99-Wear a mask. Help slow the spread of Covid-19. 2020-07-31.mp3',
 		                                     'test warning index date files_downloadDirNotExist_dic.txt']
 		playlistDirExpectedFileNameLstDic[self.playlistDirName_9] = playlistDir_9_expectedFileNameLst
 
-		playlistDir_10_expectedFileNameLst = ['99-Shmeksss Short Video 2021-01-26.mp3',
+		playlistDir_10_expectedFileNameLst = ['99-Wear a mask. Help slow the spread of Covid-19. 2020-07-31.mp3',
 		                                     'test warning index date files_downloadDirEmpty_dic.txt']
 		playlistDirExpectedFileNameLstDic[self.playlistDirName_10] = playlistDir_10_expectedFileNameLst
 
