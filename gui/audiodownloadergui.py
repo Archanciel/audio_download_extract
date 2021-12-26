25fa11af501bb3ff5b82fb492480b60b53d0bef8
Archanciel <jp.schnyder@gmail.com>
2021-12-26 20:25:13 +0100

    renamed.. GUI working. All but 12 tsts ok.

--- gui/audiodownloadergui.py
+++ gui/audiodownloadergui.py
@@ -66,13 +66,16 @@ NO_INTERNET = False
 class WindowManager(ScreenManager):
 	pass
 
-class MyRecycleView(RecycleView):
+class RecycleViewKivyBugFix(RecycleView):
+	"""
+	This class fixes a Kivy bug which sometimes causes an app failure when the
+	first selected item in a displayed list is the last item.
+	"""
 	def on_scroll_stop(self, touch, check_children=True):
 		try:
 			super().on_scroll_stop(touch, check_children)
 		except IndexError as e:
-			audioDownloaderGUI = self.parent.parent.parent
-			
+			# avoids app failure due to the uncaught IndexError exception
 			logging.info('Selecting the last list item caused this Kivy bug: ' + str(e))
 	
 class AudioDownloadSelectableRecycleBoxLayout(SelectableRecycleBoxLayout):
